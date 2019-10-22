dbf6bbf5c345e8bc1885f2c2a086dafab589ebc0
Christopher Sullivan <S00188845@mail.itsligo.ie>
2019-10-22 12:23:03 +0100

    Add project files.

---
+++ Practice CA1/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_CA1
+{
+    public class Player
+    {
+        public  int PlayerId  { get; set; }
+        public int PlayerName { get; set; }
+        public int Score { get; set; }
+
+
+    }
+}
